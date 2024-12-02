ea2b4da7c008252210d218175a2b233388efd93d
Ikeda Shunya <yukidaru.vov@gmail.com>
2024-12-03 06:51:21 +0900

    frist commit

---
+++ src/main/java/com/example/Budget/Management/Application.java
@@ -0,0 +1,13 @@
+package com.example.Budget.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
