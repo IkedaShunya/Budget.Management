de8c6e91fdd0771197ec662cc8d7fa34ca9efca6
IkedaShunya <yukidaru.vov@gmail.com>
2025-01-08 08:52:22 +0900

    gitignoreの追加

---
+++ src/main/java/com/example/Budget/Management/controller/prebudgetController.java
@@ -0,0 +1,20 @@
+package com.example.Budget.Management.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 事前予算入力用
+ *
+ */
+@Controller
+@RequestMapping("/prebudget")
+public class prebudgetController {
+    @GetMapping("/input")
+    public String inputdisplay(){
+        return "projectedexpend/preinput";
+    }
+
+
+}
